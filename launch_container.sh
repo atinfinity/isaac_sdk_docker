0683a260fe803dd4576686c1c1b651ea8781e504
atinfinity <dandelion1124@gmail.com>
2019-05-17 22:47:52 +0900

    +launch_container.sh

---
+++ launch_container.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run --runtime=nvidia --rm -it \
+           --volume=$XSOCK:$XSOCK:rw \
+           --volume=$XAUTH:$XAUTH:rw \
+           --env="XAUTHORITY=${XAUTH}" \
+           --env="DISPLAY" \
+           --net=host \
+           -u "isaac"  \
+           isaac:latest \
+           bash
