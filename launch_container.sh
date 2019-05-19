7adc64f08e6addee58599422b6b63b99cb198eb9
atinfinity <dandelion1124@gmail.com>
2019-05-19 13:41:37 +0900

    updated launch_container.sh

--- launch_container.sh
+++ launch_container.sh
@@ -5,11 +5,16 @@ XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-docker run --runtime=nvidia --rm -it \
+docker run --runtime=nvidia --privileged --rm -it \
            --volume=$XSOCK:$XSOCK:rw \
            --volume=$XAUTH:$XAUTH:rw \
+           --volume=/dev:/dev:rw \
+           --device=/dev/video0:/dev/video0:mwr \
+           --device=/dev/video1:/dev/video1:mwr \
            --env="XAUTHORITY=${XAUTH}" \
-           --env="DISPLAY" \
+           --env="DISPLAY=${DISPLAY}" \
+           --env=TERM=xterm-256color \
+           --env=QT_X11_NO_MITSHM=1 \
            --net=host \
            -u "isaac"  \
            isaac_sdk:latest \
